55b15e47d0e63fc7f57d24203689d5be00740e1a
Aaron Johnson <ajohnson13.aj@gmail.com>
2014-09-23 13:53:46 -0500

    logger bug fix

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var http = require('http');
-var connect = require('connect');
+var connect = require('connect'), serveStatic = require('serve-static');
 var directory = 'public';
+var logger = require('morgan');
 var port = process.env.PORT;
 
 var app = connect()
-  .use(connect.logger(':remote-addr -> :method :url [:status]'))
-  .use(connect.static(directory));
+  .use(logger(':remote-addr -> :method :url [:status]'))
+  .use(serveStatic(directory));
 
 http.createServer(app).listen(port, function(){
   console.log('Node server listening. Port: ' + port + ', Directory: ' + directory);
